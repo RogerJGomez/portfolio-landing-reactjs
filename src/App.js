73d89c8b776c4cb63832de2c8c46d6ad6fdd0889
RogerJGomez <gomez.roger779@gmail.com>
2020-02-17 13:10:36 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {Layout, Header, Content, Navigation, Drawer} from 'react-mdl'
+import Main from './components/Main'
+import {Link} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Roger Gómez" scroll>
+            <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer>
+            <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main />
+        </Content>
+    </Layout>
+</div>
   );
 }
 
---
+++ src/components/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <h3>
+           Home Page
+        </h3>
+    )
+}
