00e765048c8ede6b4c2e822c12f36a1bd1e2574f
RogerJGomez <gomez.roger779@gmail.com>
2020-03-01 11:25:55 -0400

    Undraggable about image

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
             <Grid className="contact-grid">
                 <Cell col={6}>
                     <Fade left>    
-                        <img className="profile-pic" src="/cartoon-rg.png" alt="profile"/>
+                        <img draggable="false" className="profile-pic" src="/cartoon-rg.png" alt="profile"/>
                     <h2>Roger Gómez</h2>
                     <p>Front-end developer from Venezuela. Currently working at an Argentinian marketing agency, <a style={{textDecoration:'none', color:'white', fontWeight:'bold'}} target="_blank" rel="noopener noreferrer" href="https://clichead.com/">Clichead</a>, 
                     where I'm in charge of developing and maintaining dozens of websites built with WordPress, for small and medium sized businesses based all over the world.</p>
